ae6b9972e473ebe5728858712b8782f726eaaffb
FosterSOAsare <fostersoasare@gmail.com>
2024-10-30 14:47:53 +0000

    Finishing

--- client/src/components/Error.tsx
+++ client/src/components/Error.tsx
@@ -1,6 +1,6 @@
 import { InboxIcon } from "lucide-react";
 
-export default function NoData({ reset }: { reset: () => void }) {
+export default function Error({ reset }: { reset: () => void }) {
 	return (
 		<div className="flex min-h-[400px] w-full items-center justify-center px-4">
 			<div className="flex flex-col items-center justify-center space-y-4 text-center" role="status" aria-live="polite">
@@ -8,12 +8,12 @@ export default function NoData({ reset }: { reset: () => void }) {
 					<InboxIcon className="h-12 w-12 text-muted-foreground" aria-hidden="true" />
 				</div>
 
-				<h2 className="text-lg font-semibold">No Data Available</h2>
+				<h2 className="text-lg font-semibold">An Error Occurred</h2>
 
-				<p className="max-w-sm text-sm text-muted-foreground">There are no items to display at the moment.</p>
+				<p className="max-w-sm text-sm text-muted-foreground">There was an error during operation</p>
 
 				<button onClick={() => reset()} className="p-3 text-white px-5 bg-tag">
-					Reset Filters and Refetch
+					Refetch
 				</button>
 			</div>
 		</div>
